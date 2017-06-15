3e22252ef8fbdd783bd4bd845632add53b713b49
Ильяс <i.almakaev@gmail.com>
2017-06-15 10:04:32 +0700

    Initial Commit

---
+++ Patterns_1_Prototype/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Patterns_1_Prototype
+//
+//  Created by Ильяс on 15.06.17.
+//  Copyright © 2017 Алмакаев Ильяс. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
